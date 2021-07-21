541b361b9222b8fda69395b13d9c7f1aad541923
lgfunderburk <laura.gutierrez.funderburk@cybera.ca>
2021-07-21 00:28:56 -0700

    modify function cleanup exercise

--- data-workflow/scripts/uk_co2_download_performance_monitor.py
+++ data-workflow/scripts/uk_co2_download_performance_monitor.py
@@ -15,6 +15,75 @@ from datetime import datetime
 import pandas as pd
 import sys
 
+def perform_query(url):
+    """
+    This function performs a query on a REST API and returns a dataframe with the content 
+    of the query
+    
+    Parameters
+    ----------
+        url (str) full URL containing the query
+        record_flag (bool)
+        
+    Returns
+    -------
+        flat_df (dataframe object) containing JSON response in dataframe format
+    """
+    
+    try:
+        # Time query
+        start_time = time.time()
+        # Using GET command 
+        response = requests.get(url)
+        total_time = time.time() - start_time
+        print(total_time)
+        # Raise issues if response is different from 200
+        response.raise_for_status()
+
+        # access JSOn content
+        jsonResponse = response.json()
+
+        return jsonResponse
+    
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+def download_intensity_by_date(start_d, end_d):
+    
+    """
+    This function downloads actual and forecasted intensity during a given time range
+    
+    Parameters
+    ----------
+    start_d (str) date in format YYYY-MM-DD indicating start of observations
+    end_d (str) date in format YYYY-MM-DD indicating end of observations
+    
+    Returns
+    -------
+    all_df (data frame object) 
+    
+    """
+    try:
+        # Create data range 
+        date_range = pd.date_range(start=start_d, end=end_d, freq='D')
+        dates = [date.date() for date in date_range]
+        # Iterate over all dates 
+        # Perform query
+        # Store content into master dataframe
+        all_df = []
+        for item in dates:
+            new_url = f'https://api.carbonintensity.org.uk/intensity/date/{item}'
+            query = perform_query(new_url)
+            df = pd.json_normalize(query, record_path="data")
+            all_df.append(df)
+
+
+        return all_df
+    except:
+        print("Error")
+
 def query_entry_pt(url):
     """This function takes as input a URL entry point and returns the complete JSON response in a REST API
     
